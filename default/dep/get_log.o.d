61285c49d594869a3b69adf97605fcd865779880
kubi48 <kubi48@6ebdd44f-5a7e-449c-b779-36259138d2c7>
2014-01-26 20:15:25 +0000

    geaenderte D-mode Erkennung und Aseembler wait_for_key_ms und get_log
    git-svn-id: svn://mikrocontroller.net/transistortester/Software/trunk@230 6ebdd44f-5a7e-449c-b779-36259138d2c7

--- default/dep/get_log.o.d
+++ default/dep/get_log.o.d
@@ -1,4 +1,4 @@
-wait_for_key_ms.o: ../wait_for_key_ms.S \
+get_log.o: ../get_log.S \
  /usr/lib/gcc/avr/4.5.3/../../../avr/include/avr/io.h \
  /usr/lib/gcc/avr/4.5.3/../../../avr/include/avr/sfr_defs.h \
  /usr/lib/gcc/avr/4.5.3/../../../avr/include/avr/iom168.h \
@@ -8,8 +8,7 @@ wait_for_key_ms.o: ../wait_for_key_ms.S \
  /usr/lib/gcc/avr/4.5.3/../../../avr/include/avr/version.h \
  /usr/lib/gcc/avr/4.5.3/../../../avr/include/avr/fuse.h \
  /usr/lib/gcc/avr/4.5.3/../../../avr/include/avr/lock.h ../config.h \
- ../autoconf.h /usr/lib/gcc/avr/4.5.3/../../../avr/include/stdlib.h \
- ../lcd_defines.h
+ ../autoconf.h /usr/lib/gcc/avr/4.5.3/../../../avr/include/stdlib.h
 
 /usr/lib/gcc/avr/4.5.3/../../../avr/include/avr/io.h:
 
@@ -34,5 +33,3 @@ wait_for_key_ms.o: ../wait_for_key_ms.S \
 ../autoconf.h:
 
 /usr/lib/gcc/avr/4.5.3/../../../avr/include/stdlib.h:
-
-../lcd_defines.h:
